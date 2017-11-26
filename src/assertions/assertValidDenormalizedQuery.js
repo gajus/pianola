50f3ede986cbda70ed4f1b5eb04a92b366f86a38
Gajus Kuizinas <gajus@gajus.com>
2017-11-26 22:11:10 +0000

    fix: update dependencies

--- src/assertions/assertValidDenormalizedQuery.js
+++ src/assertions/assertValidDenormalizedQuery.js
@@ -16,10 +16,10 @@ const validate = ajv.compile(denormalizedQueryShema);
 export default (denormalizedQuery: DenormalizedQueryType, log: boolean = true): void => {
   if (!validate(denormalizedQuery)) {
     if (log) {
-      // eslint-disable-next-line
+      // eslint-disable-next-line no-console
       console.log('query', denormalizedQuery);
 
-      // eslint-disable-next-line
+      // eslint-disable-next-line no-console
       console.error('Validation errors', validate.errors);
     }
 
